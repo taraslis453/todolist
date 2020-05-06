7414e8770daca31806e6d37ca06ed162e54b997f
taraslis453 <taraslisiy54@gmail.com>
2020-05-06 23:50:29 +0300

    beta adaptive vh height mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 const listItems = document.querySelector('.list__items');
 listItems.innerHTML = localStorage.getItem('todoList');
 
