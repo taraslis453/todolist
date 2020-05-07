a1af9a09465178851246909c3d104ddea9aaffea
taraslis453 <taraslisiy54@gmail.com>
2020-05-07 17:35:59 +0300

    Добавил вычисление количества задач и вывод их возле названия списка

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function createListDiv() {
     listDiv.innerHTML =
         `<svg class="delete__list-icon" fill="#000000" width="26px" height="26px"><path d="M 21.734375 19.640625 L 19.636719 21.734375 C 19.253906 22.121094 18.628906 22.121094 18.242188 21.734375 L 13 16.496094 L 7.761719 21.734375 C 7.375 22.121094 6.746094 22.121094 6.363281 21.734375 L 4.265625 19.640625 C 3.878906 19.253906 3.878906 18.628906 4.265625 18.242188 L 9.503906 13 L 4.265625 7.761719 C 3.882813 7.371094 3.882813 6.742188 4.265625 6.363281 L 6.363281 4.265625 C 6.746094 3.878906 7.375 3.878906 7.761719 4.265625 L 13 9.507813 L 18.242188 4.265625 C 18.628906 3.878906 19.257813 3.878906 19.636719 4.265625 L 21.734375 6.359375 C 22.121094 6.746094 22.121094 7.375 21.738281 7.761719 L 16.496094 13 L 21.734375 18.242188 C 22.121094 18.628906 22.121094 19.253906 21.734375 19.640625 Z"/></svg>
         <input type="text" class="list__item-name" value="Untitled list">
-        <div class="list__item-count">${1}</div>`;
+        <div class="list__item-count"></div>`;
     listItems.append(listDiv);
     listen();
     selectLastListDiv();
@@ -135,7 +135,7 @@ function listen() {
                 listItemInputs[i].blur();
                 listItemInputs[i].readOnly = true;
                 let listItemPersonalName = listItemInputs[i].parentNode.classList[1];
-                let selectedList  = taskItems.querySelector('.' + listItemPersonalName);
+                selectedList  = taskItems.querySelector('.' + listItemPersonalName);
                 let selectedListTitle = selectedList.querySelector('.list__item-title');
                 selectedListTitle.innerHTML = listItemInputs[i].value;
                 aside.classList.add('d-mob-none');
@@ -247,6 +247,7 @@ function createTask(e) {
     taskItems.querySelector('.' + listSelectedName).append(task);
     // Очистка инпута после добавление задачи
     e.value = "";
+    listItems.querySelector('.' + listSelectedName + " " + '.list__item-count').innerHTML = taskItems.querySelectorAll('.' + listSelectedName + " " + '.task').length;
     saveToStorage();
 }
 
