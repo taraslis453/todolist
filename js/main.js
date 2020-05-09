043f2d73c8a9ac532f60365201434432273840ef
taraslis453 <taraslisiy54@gmail.com>
2020-05-09 12:49:33 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function createListDiv() {
     listDiv.innerHTML =
         `<svg class="delete__list-icon" fill="#000000" width="26px" height="26px"><path d="M 21.734375 19.640625 L 19.636719 21.734375 C 19.253906 22.121094 18.628906 22.121094 18.242188 21.734375 L 13 16.496094 L 7.761719 21.734375 C 7.375 22.121094 6.746094 22.121094 6.363281 21.734375 L 4.265625 19.640625 C 3.878906 19.253906 3.878906 18.628906 4.265625 18.242188 L 9.503906 13 L 4.265625 7.761719 C 3.882813 7.371094 3.882813 6.742188 4.265625 6.363281 L 6.363281 4.265625 C 6.746094 3.878906 7.375 3.878906 7.761719 4.265625 L 13 9.507813 L 18.242188 4.265625 C 18.628906 3.878906 19.257813 3.878906 19.636719 4.265625 L 21.734375 6.359375 C 22.121094 6.746094 22.121094 7.375 21.738281 7.761719 L 16.496094 13 L 21.734375 18.242188 C 22.121094 18.628906 22.121094 19.253906 21.734375 19.640625 Z"/></svg>
         <input type="text" class="list__item-name" value="Untitled list">
-        <div class="list__item-count"></div>`;
+        <div class="list__item-count">0</div>`;
     listItems.append(listDiv);
     listen();
     selectLastListDiv();
@@ -227,6 +227,7 @@ function listen() {
     for (let i = 0; i < allDeleteTaskIcons.length; i++) {
         allDeleteTaskIcons[i].addEventListener('click', () => {
             allDeleteTaskIcons[i].parentNode.remove();
+            listItems.querySelector('.' + listSelectedName + " " + '.list__item-count').innerHTML = taskItems.querySelectorAll('.' + listSelectedName + " " + '.task').length;
             saveToStorage();
         })
     }
@@ -259,7 +260,7 @@ function createTask(e) {
         e.classList.remove('wrong')
     }
     // Получаю список который выбран и передаю в инпут для добавление задач что бы он знал куда добавлять
-    let listSelectedName = localStorage.getItem('listSelectedName');
+    listSelectedName = localStorage.getItem('listSelectedName');
     // Создание задачи с уникальным именем
     let task = document.createElement('div');
     task.classList.add('task');
